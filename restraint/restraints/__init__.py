c339c500af97f2697dbced308887fc0f79b2aca4
Kyle Walker <KyleJamesWalker@gmail.com>
2020-03-15 10:15:25 -0700

    Create examples folder

--- restraint/restraints/__init__.py
+++ restraint/restraints/__init__.py
@@ -8,7 +8,7 @@ class Limit:
     """Simple time based limit
 
     This restraint will limit the number of calls in a given time period, and
-    will reset at the top of each period.   This will allow to use all your
+    will reset at the top of each period. This will allow to use all your
     quota as fast as possible in each given time period.
 
     Args:
@@ -113,54 +113,3 @@ class Limit:
         for key, value in self.rate_remaining.items():
             if self.rate_remaining[key] >= 1:
                 self.rate_remaining[key] -= 1
-
-
-class StartupTimeAwareLimit(Limit):
-    def __init__(
-        self,
-        second=0, minute=0, hour=0, day=0, month=0, year=0,
-    ):
-        """Simple rate limiter that reduces the init rate based current time
-
-        Args:
-            second: Per second rate
-            minute: Per minute rate
-            hour: Per hour rate
-            day: Per day rate
-            month: Per month rate
-            year: Per year rate
-
-        """
-        super().__init__(
-            second=second, minute=minute, hour=hour,
-            day=day, month=month, year=year,
-        )
-
-        # This is wrong right now
-        """
-        62 calls a month
-        means about 2 call per day in a 31 day month and 2.214 calls in a 28 mo
-        31 - ((2--2.214) * cur_day_number)
-
-        TODO: MAKE A TEST
-        """
-        cur_month_days = calendar.monthrange(
-            self.dt_info.year, self.dt_info.month
-        )[1]
-        cur_year_days = 366 if calendar.isleap(self.dt_info.year) else 365
-        self.rate_remaining = {
-            'second': int(
-                self.second * (self.dt_info.microsecond / 1000000) + 1
-                ),
-            'minute': int(self.minute * (self.dt_info.second / 60) + 1),
-            'hour': int(self.hour * (self.dt_info.minute / 60) + 1),
-            'day': int(self.day * (self.dt_info.hour / 24) + 1),
-            'month': int(
-                self.month * (self.dt_info.day / cur_month_days) + 1
-            ),
-            'year': int(
-                self.year * (
-                    self.dt_info.timetuple().tm_yday / cur_year_days
-                ) + 1
-            ),
-        }

eaeedac829997427048268b54f77296c6d6db0ea
Kyle Walker <KyleJamesWalker@gmail.com>
2020-03-15 12:28:02 -0700

    Travis Support

---
+++ Makefile
@@ -0,0 +1,21 @@
+debug ?= false
+
+ifeq ($(debug),true)
+	test_extra_params := -- --pudb
+else
+	test_extra_params :=
+endif
+
+clean:
+	@rm -rf build/ .tox/ .eggs/ .pytest_cache/ *.egg-info *.egg coverage.xml
+	@find . -name '*.pyc' -delete
+	@find . -name '__pycache__' -delete
+
+test3:
+	-tox -e py36 $(test_extra_params)
+
+test:
+	tox -p auto
+
+build:
+	PYENV_VERSION=rest python setup.py build
---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import find_packages, setup
+
+readme = open('README.rst').read()
+
+requirements = {
+    "package": [
+        "PyYAML>=5",
+    ],
+    "test": [
+        "pytest",
+        "pytest-cov",
+        "pytest-flake8",
+        "pytest-mock",
+        "pytest-pudb",
+    ],
+    "setup": [
+        "pytest-runner",
+    ],
+}
+
+requirements.update(all=sorted(set().union(*requirements.values())))
+
+setup(
+    name='restraint',
+    version='0.0.1-rc1',
+    description='Rate Limiting Module',
+    long_description=readme,
+    author='Kyle James Walker',
+    author_email='KyleJamesWalker@gmail.com',
+    url='https://github.com/KyleJamesWalker/restraint-py',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=requirements['package'],
+    extras_require=requirements,
+    setup_requires=requirements['setup'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    test_suite='tests',
+    tests_require=requirements['test'],
+)
